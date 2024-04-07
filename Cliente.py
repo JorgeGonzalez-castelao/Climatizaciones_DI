2ebf71755e0f89b034bac4cd75dac9ed873466be
JorgeGonzalez-castelao <jgonzalez@danielcastelao.org>
2024-04-07 23:11:05 +0200

    Clases , Objetos y Acceso a BD (Sujeto a Cambios)

---
+++ Cliente.py
@@ -0,0 +1,18 @@
+class Cliente:
+    def __init__(self, nombre, apellido, telefono, direccion):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.telefono = telefono
+        self.direccion = direccion
+
+    def insertar_cliente(self):
+        # Lógica para insertar el cliente en la base de datos
+        pass
+
+    def consultar_cliente(self):
+        # Lógica para consultar un cliente en la base de datos
+        pass
+
+    def borrar_cliente(self):
+        # Lógica para borrar un cliente de la base de datos
+        pass
---
+++ informe.py
@@ -0,0 +1,7 @@
+class Informe:
+    def __init__(self, tipo):
+        self.tipo = tipo
+
+    def generar_informe(self):
+        # Lógica para generar el informe utilizando Reportlab
+        pass
